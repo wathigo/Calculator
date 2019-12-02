2adf0f2c4dc2a17ac39ba8e137b8ef9093ab97b9
wathigo <wathigosimon@gmail.com>
2019-12-01 20:57:03 -0500

    Update ButtonPanel component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import ButtonPanel from './ButtonPanel';
+
 function App() {
   return (
     <div className="App">
-      <h1> Welcome to React Calculator </h1>
+      <ButtonPanel/>
     </div>
   );
 }
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Button from './Button'
+
+
+class ButtonPanel extends Component{
+  render(){
+    return(
+      <div>
+        <div className='group1'>
+         <Button content='AC'/>
+         <Button content='+/-'/>
+         <Button content='X'/>
+         <Button content='/'/>
+        </div>
+        <div className='group2'>
+         <Button content='7'/>
+         <Button content='8'/>
+         <Button content='9'/>
+         <Button content='X'/>
+        </div>
+        <div className='group3'>
+         <Button content='4'/>
+         <Button content='5'/>
+         <Button content='6'/>
+         <Button content='-'/>
+        </div>
+        <div className='group4'>
+         <Button content='1'/>
+         <Button content='2'/>
+         <Button content='3'/>
+         <Button content='+'/>
+        </div>
+        <div className='group5'>
+         <Button content='0'/>
+         <Button content='.'/>
+         <Button content='='/>
+        </div>
+      </div>
+      );
+  }
+}
+
+export default ButtonPanel;
