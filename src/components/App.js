a085ccd37998971de84fc3b5542a2f356873c030
wathigo <wathigosimon@gmail.com>
2019-12-11 07:02:26 -0500

    Render Display component in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 import ButtonPanel from './ButtonPanel';
+import Display from './Display';
 
 function App() {
   return (
     <div className="App">
+      <Display />
       <ButtonPanel/>
     </div>
   );
