726751029f0d19ff1199512c415d0a46441d8b21
wathigo <wathigosimon@gmail.com>
2019-12-31 06:47:46 -0500

    Use map method to keep the code dry

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -9,36 +9,45 @@ import Button from './Button';
 class ButtonPanel extends Component {
   render() {
     const { clickHandler } = this.props;
+    const group1 = ['AC', '+/-', 'X', '÷'].map((val) => <Button key={val} handleClick={clickHandler} content={val} color="cl-light-grey" />);
+    const group2 = ['7', '8', '9', 'X'].map((val) => <Button key={val} handleClick={clickHandler} content={val} color="cl-light-grey" />);
+    const group3 = ['4', '5', '6', '-'].map((val) => <Button key={val} handleClick={clickHandler} content={val} color="cl-light-grey" />);
+    const group4 = ['1', '2', '3', '+'].map((val) => <Button key={val} handleClick={clickHandler} content={val} color="cl-light-grey" />);
+    const group5 = ['0', '.', '='].map((val, index) => {
+      if (index !== 2) {
+        if (val === '0') {
+          return <Button key={val} handleClick={clickHandler} content={val} color="cl-light-grey w-50" />;
+        }
+        return <Button key={val} handleClick={clickHandler} content={val} color="cl-light-grey" />;
+      }
+      return <Button key={val} handleClick={clickHandler} content={val} />;
+    });
     return (
       <div className="button-panel">
         <div className="group1">
-          <Button handleClick={clickHandler} content="AC" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="+/-" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="X" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="÷" />
+          {
+            group1
+          }
         </div>
         <div className="group2">
-          <Button handleClick={clickHandler} content="7" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="8" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="9" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="X" />
+          {
+          group2
+        }
         </div>
         <div className="group3">
-          <Button handleClick={clickHandler} content="4" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="5" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="6" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="-" />
+          {
+          group3
+        }
         </div>
         <div className="group4">
-          <Button handleClick={clickHandler} content="1" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="2" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="3" color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="+" />
+          {
+          group4
+        }
         </div>
         <div className="group5">
-          <Button handleClick={clickHandler} content="0" wide color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="." color="cl-light-grey" />
-          <Button handleClick={clickHandler} content="=" />
+          {
+          group5
+        }
         </div>
       </div>
     );
